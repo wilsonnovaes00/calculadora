73cde0e917b18594b4dff047372bf8f58a235b22
Wilson Pereira Novaes Filho <wilsonnovaes00@hotmail.com>
2017-08-12 22:23:57 -0300

    Create component on folder calculadora

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { CalculadoraModule } from './calculadora/calculadora.module';
+import { CalculadoraModule } from './calculadora';
 
 @NgModule({
   declarations: [
--- src/app/calculadora/calculadora.module.ts
+++ src/app/calculadora/calculadora.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CalculadoraComponent } from './calculadora/calculadora.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [CalculadoraComponent]
 })
 export class CalculadoraModule { }
